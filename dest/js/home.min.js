4ebd7d8ec68c8890f4e59237c6ffd006d8e481e1
weraleto <destiny-smile@mail.ru>
2020-03-05 17:39:09 +0300

    footer gallery + some fixes

--- dest/js/home.min.js
+++ dest/js/home.min.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+// block 3 gallery animation
+
     $('.section-protect_item').on('mouseover',(e)=>{
         let startWidth=$(document).width()/8;
      $('.section-protect_item').css({'width':startWidth+'px'});
@@ -8,23 +10,27 @@ $(document).ready(function () {
         $(imgBlock).css({'width' : startWidth+(startWidth*0.14)*7+'px'});
         $(imgBlock).find('.item-gallery_overlay').addClass('hide-overlay');
         $(imgBlock).find('.item-gallery_intro').removeClass('hide-bl');
+        $(imgBlock).find('canvas').css({'opacity':'1'});
 
 
         let currId=imgBlock.id;
 
-        $('#'+imgBlock.id+' canvas').css({'opacity':'1'});
+        // $('#'+imgBlock.id+' canvas').css({'opacity':'1'});
         let canvas = $('#'+imgBlock.id+' canvas')[0].getContext('2d');
         canvas.width=100;
         canvas.height=100;
-        canvas.clearRect(0,0,100,100);
+        canvas.clearRect(0,0,120,100);
 
         let canvasImage= $(imgBlock).find('img')[0];
         let frames=1;
 
-        setInterval(()=>{
-            drawImage (canvasImage.src,frames);
+        let iconAnimation=setInterval(()=>{
+            drawImage (canvasImage,frames,canvas);
             frames++;
-        },150)
+            if(frames==9){clearInterval(iconAnimation)}
+        },150);
+
+        
 
 
         $(e.target).on('mouseout',()=>{
@@ -32,22 +38,71 @@ $(document).ready(function () {
             $('.item-gallery_overlay').removeClass('hide-overlay');
             $('.item-gallery_intro').addClass('hide-bl');
             $('canvas').css({'opacity':'0'});
+            clearInterval(iconAnimation);
         })
     })
     $('.section-protect_item').on('resize',(e)=>{
         $('.section-protect_item').css({'width' : $(document).width()/8+'px'});
     });
     
-    function drawImage(img,num){
+    function drawImage(img,num,canvas){
         num=num?num-1:0;
-        canvasImage.onload = function() {
+        img.onload = function() {
             width = 98;
             height = 82;
-            canvas.drawImage(canvasImage,0,834*num,834,834,12,0,98,82);
+            canvas.drawImage(img,0,834*num,834,834,12,0,98,82);
 
         }
-        canvasImage.src=img;
+        img.src=img.src;
     }
+
+
+
+    $('.footer-gallery_item').on('mouseover',(e)=>{
+        let iden=$(e.target).closest('.footer-gallery_item').data('quality');
+        // let params={six:[['one',]]};
+
+        
+        if(iden=='one') {
+            $('.footer-gallery_item').css({'transform':'translate(20px,20px)'});
+            $('.quality_six, .quality_seven').css({'transform':'translateY(0)'});
+            $('.footer-gallery_photo-one').css({'transform':'translate(-20px,-20px)'});
+            $('.footer-gallery_photo-two').css({'transform':'translateX(10px)'});
+            $('.footer-gallery_photo-six').css({'transform':'translateX(20px)'});
+            
+        }
+        if(iden=='two'){
+            $('.footer-gallery_item').css({'transform':'translateY(20px)'});
+            $('.quality_six, .quality_seven').css({'transform':'translate(20px,0)'});
+            $('.quality_one, .quality_three, .quality_four').css({'transform':'translate(-20px,20px)'});
+        }
+
+        if(iden=='three'||iden=='four') {
+            $('.footer-gallery_item').css({'transform':'translate(20px,-20px)'});
+            $('.quality_six, .quality_seven').css({'transform':'translateY(0)'});
+            $('.footer-gallery_photo-one').css({'transform':'translate(-20px,-20px)'});
+            $('.footer-gallery_photo-two').css({'transform':'translateX(20px)'});
+            $('.footer-gallery_photo-six').css({'transform':'translateX(20px)'});
+            
+        }
+        if(iden=='five'){
+            $('.footer-gallery_item').css({'transform':'translateY(-20px)'});
+            $('.quality_six, .quality_seven').css({'transform':'translate(20px,0)'});
+            $('.quality_one, .quality_three, .quality_four').css({'transform':'translate(-20px,-20px)'});
+        }
+        
+        if(iden=='six'||iden=='seven'){
+            $('.footer-gallery_item').css({'transform':'translateX(-20px)'});
+            $('.footer-gallery_photo-one').css({'transform':'translateX(-20px)'});
+            $('.footer-gallery_photo-two').css({'transform':'translateX(-20px)'});
+            $('.footer-gallery_photo-six').css({'transform':'translateX(20px)'});
+        }
+
+        $('.footer-gallery_item').on('mouseout',(e)=>{
+            $('.footer-gallery_item').css({'transform':'initial'});
+            $('.footer-gallery_photo').css({'transform':'initial'});
+        })
+    })
 
 });
 
